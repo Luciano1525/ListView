196b2a8f37b2a454cba00a45375104bc551f9bf3
Luciano Bacab <lucianobacab427@gmail.com>
2023-05-27 13:41:51 -0600

    Primer Commit

---
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView TV;
+    private ListView LV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
